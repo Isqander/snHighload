57f5e86db7484daf2063081551f8e87e0e308b06
isqander <isqander@gmail.com>
2022-07-04 08:37:04 +0300

    init

---
+++ src/test/kotlin/com/example/highloadsn/HighloadSnApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.highloadsn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HighloadSnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
